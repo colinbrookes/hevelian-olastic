299f072996c1ec7f0c159aa1f254eef3013efc82
RuslanDidyk <junglist.ruslan@gmail.com>
2017-03-09 14:08:30 +0200

    Fixed filtering by _id property

--- olastic-core/src/main/java/com/hevelian/olastic/core/api/uri/queryoption/expression/member/impl/PrimitiveMember.java
+++ olastic-core/src/main/java/com/hevelian/olastic/core/api/uri/queryoption/expression/member/impl/PrimitiveMember.java
@@ -1,12 +1,15 @@
 package com.hevelian.olastic.core.api.uri.queryoption.expression.member.impl;
 
+import static com.hevelian.olastic.core.elastic.ElasticConstants.ID_FIELD_NAME;
 import static com.hevelian.olastic.core.elastic.utils.ElasticUtils.addKeywordIfNeeded;
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.idsQuery;
 import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 import org.apache.olingo.commons.api.edm.EdmType;
 import org.apache.olingo.server.api.ODataApplicationException;
+import org.elasticsearch.index.query.QueryBuilder;
 
 import com.hevelian.olastic.core.api.uri.queryoption.expression.member.ExpressionMember;
 
@@ -23,15 +26,12 @@ public class PrimitiveMember extends TypedMember {
 
     @Override
     public ExpressionResult eq(ExpressionMember expressionMember) throws ODataApplicationException {
-        return new ExpressionResult(termQuery(addKeywordIfNeeded(getField(), getEdmType()),
-                ((LiteralMember) expressionMember).getValue()));
+        return new ExpressionResult(getEqQuery(expressionMember));
     }
 
     @Override
     public ExpressionResult ne(ExpressionMember expressionMember) throws ODataApplicationException {
-        return new ExpressionResult(
-                boolQuery().mustNot(termQuery(addKeywordIfNeeded(getField(), getEdmType()),
-                        ((LiteralMember) expressionMember).getValue())));
+        return new ExpressionResult(boolQuery().mustNot(getEqQuery(expressionMember)));
     }
 
     @Override
@@ -56,5 +56,21 @@ public class PrimitiveMember extends TypedMember {
     public ExpressionResult lt(ExpressionMember expressionMember) throws ODataApplicationException {
         return new ExpressionResult(
                 rangeQuery(getField()).lt(((LiteralMember) expressionMember).getValue()));
+    }
+
+    /**
+     * Gets query for equals and not equals operations.
+     * 
+     * @param expressionMember
+     *            member with value
+     * @return appropriate query
+     */
+    private QueryBuilder getEqQuery(ExpressionMember expressionMember) {
+        Object value = ((LiteralMember) expressionMember).getValue();
+        if (getField().equals(ID_FIELD_NAME)) {
+            return idsQuery().addIds(value.toString());
+        } else {
+            return termQuery(addKeywordIfNeeded(getField(), getEdmType()), value);
+        }
     }
 }
