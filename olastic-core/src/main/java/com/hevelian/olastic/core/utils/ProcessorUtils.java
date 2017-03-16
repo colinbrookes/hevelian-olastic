5ce9168f3a0843db2952840882acb3d5cd95aae1
tkoh <tarasrng@gmail.com>
2017-03-16 18:56:32 +0200

    encode id before creating URI

--- olastic-core/src/main/java/com/hevelian/olastic/core/utils/ProcessorUtils.java
+++ olastic-core/src/main/java/com/hevelian/olastic/core/utils/ProcessorUtils.java
@@ -1,12 +1,15 @@
 package com.hevelian.olastic.core.utils;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Locale;
-
 import org.apache.olingo.commons.api.ex.ODataRuntimeException;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.ODataApplicationException;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
 /**
  * Contains utility methods.
@@ -29,8 +32,15 @@ public final class ProcessorUtils {
      */
     public static URI createId(String entitySetName, Object id) {
         try {
-            return new URI(entitySetName + "(" + id + ")");
-        } catch (URISyntaxException e) {
+            URI uri;
+            Object escapedId = id;
+            if (id instanceof String) {
+                escapedId = URLEncoder.encode((String)id, StandardCharsets.UTF_8.name());
+            }
+            uri = new URI(entitySetName + "(" + escapedId + ")");
+            return uri;
+
+        } catch (URISyntaxException | UnsupportedEncodingException e) {
             throw new ODataRuntimeException("Unable to create id for entity: " + entitySetName, e);
         }
     }
