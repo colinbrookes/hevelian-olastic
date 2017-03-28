a1d2f835a3630005c97265fc732dd303e3bb6884
tkoh <tarasrng@gmail.com>
2017-03-28 15:16:40 +0300

    implement multiple entity sets in response parsers

--- olastic-core/src/main/java/com/hevelian/olastic/core/elastic/parsers/ESResponseParser.java
+++ olastic-core/src/main/java/com/hevelian/olastic/core/elastic/parsers/ESResponseParser.java
@@ -1,10 +1,11 @@
 package com.hevelian.olastic.core.elastic.parsers;
 
+import com.hevelian.olastic.core.edm.ElasticEdmEntitySet;
+import com.hevelian.olastic.core.processors.data.InstanceData;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.elasticsearch.action.search.SearchResponse;
 
-import com.hevelian.olastic.core.edm.ElasticEdmEntitySet;
-import com.hevelian.olastic.core.processors.data.InstanceData;
+import java.util.List;
 
 /**
  * Interface to provide behavior for Elasticsearch response parsers.
@@ -31,6 +32,23 @@ public interface ESResponseParser<T, V> {
      *             if any error occurred during parsing response
      */
     InstanceData<T, V> parse(SearchResponse response, ElasticEdmEntitySet entitySet)
+            throws ODataApplicationException;
+
+    /**
+     * Parses Elasticsearch {@link SearchResponse} and returns
+     * {@link InstanceData} with type and value.
+     *
+     * @param response
+     *            response from Elasticsearch
+     * @param responseEntitySets
+     *            entitySet list of the data returned from Elasticsearch
+     * @param returnEntitySet
+     *            entitySet that should be returned to the client
+     * @return instance data with type and value
+     * @throws ODataApplicationException
+     *             if any error occurred during parsing response
+     */
+    InstanceData<T, V> parse(SearchResponse response, List<ElasticEdmEntitySet> responseEntitySets, ElasticEdmEntitySet returnEntitySet)
             throws ODataApplicationException;
 
 }
--- olastic-core/src/main/java/com/hevelian/olastic/core/elastic/parsers/SingleResponseParser.java
+++ olastic-core/src/main/java/com/hevelian/olastic/core/elastic/parsers/SingleResponseParser.java
@@ -1,8 +1,16 @@
 package com.hevelian.olastic.core.elastic.parsers;
 
+import com.hevelian.olastic.core.edm.ElasticEdmEntitySet;
 import com.hevelian.olastic.core.edm.ElasticEdmEntityType;
 import com.hevelian.olastic.core.edm.PropertyCreator;
+import com.hevelian.olastic.core.processors.data.InstanceData;
 import org.apache.olingo.commons.api.data.Property;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.elasticsearch.action.search.SearchResponse;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Abstract parser with common behavior for all parsers.
@@ -27,5 +35,16 @@ public abstract class SingleResponseParser<T, V> implements ESResponseParser<T, 
      */
     protected Property createProperty(String name, Object value, ElasticEdmEntityType entityType) {
         return propertyCreator.createProperty(name, value, entityType);
+    }
+
+    public InstanceData<T, V> parse(SearchResponse response, List<ElasticEdmEntitySet> responseEntitySets, ElasticEdmEntitySet returnEntitySet)
+            throws ODataApplicationException {
+        return parse(response, returnEntitySet);
+    }
+
+    public InstanceData<T, V> parse(SearchResponse response, ElasticEdmEntitySet entitySet)
+            throws ODataApplicationException {
+        throw new ODataApplicationException("Not implemented", HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(),
+                Locale.ROOT);
     }
 }
