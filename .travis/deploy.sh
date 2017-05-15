6ba44d770f928f7e98f28e53369c6588fceba0db
tkoh <tarasrng@gmail.com>
2017-05-15 11:55:06 +0300

    fix travis build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,21 +7,9 @@ then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis/gpg.sh
-    
     mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
 # do deployment to snapshot repo
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
     mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots
-fi
-
-if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
 fi
